2848ba9b8631ec2f41eff9c208897a3a3f4c410a
aefiq96 <g00324934@gmit.ie>
2019-03-14 13:33:12 +0000

    attached scripts to gun and enemy

---
+++ Assets/Scipts/HandGunDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunDamage : MonoBehaviour
+{
+    int DamageAmount = 5;
+    float TargetDistance;
+    float AllowedRange = 15;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            RaycastHit Shot;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),out Shot))
+            {
+                TargetDistance = Shot.distance;
+                if (TargetDistance < AllowedRange)
+                {
+                    Shot.transform.SendMessage("DeductPoints", DamageAmount);
+                }
+            }
+        }
+    }
+}
